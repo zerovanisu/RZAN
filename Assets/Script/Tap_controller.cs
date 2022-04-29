a1164a9f47e7b01da37034e5d819d8a3d4830845
zerovanisu <mazinari310@gmail.com>
2022-04-29 17:58:08 +0900

    タップエフェクト追加

--- Assets/Script/Tap_controller.cs
+++ Assets/Script/Tap_controller.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Tap_Controller : MonoBehaviour
 {
-    [Header("エフェクト")]
-    [SerializeField]
-    private ParticleSystem Tap_Effect;
+    [SerializeField, Header("エフェクト")]
+    GameObject Tap_Effect;
 
-    [Header("カメラ")]
-    [SerializeField]
+    ParticleSystem[] Effect_Children;
+
+    [SerializeField, Header("カメラ")]
     private Camera Game_Camera;
 
     void Start()
     {
-        
+        Effect_Children = Tap_Effect.GetComponentsInChildren<ParticleSystem>();
     }
 
     void Update()
     {
-        var pos = Input.mousePosition;
-        pos.z = 10f[=]
+        if(Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+        {
+            var pos = Input.mousePosition;
+            pos.z = 10f;
+
+            transform.position = Game_Camera.ScreenToWorldPoint(pos);
+
+            for (int i = 0; i < Effect_Children.Length; i++)
+            {
+                Effect_Children[i].Play();
+            }
+        }
     }
 }
